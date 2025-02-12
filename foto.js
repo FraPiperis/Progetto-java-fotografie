9e0d43058c3c8d2ff51d67f850f17d730e4996db
FraPiperis <FrancescaPiperis_00@outlook.it>
2025-02-12 19:09:53 +0100

    mod  2

--- foto.js
+++ foto.js
@@ -1,13 +1,33 @@
-const CatFetch = "https://api.pexels.com/v1/search?query=cat"
 
-function foto() {
-     fetch (CatFetch, {
-        headers: {
-            Authorization: "RxV2jTZgbdTVCkBj0Vhqsfea64Z4NYrk2k3EQAR73MvmlKGEVWZVVfXf"
-        }
-    } )
-    .then (Response => Response.json())
-    .then((img) => renderFoto(img.photos))
-    .catch(err => console.log(err))
+const FunzioneGatto = "https://api.pexels.com/v1/search?query=cat"
+function funzionefetch() { 
+    return fetch (FunzioneGatto, {
+    headers: {
+        Authorization: "RxV2jTZgbdTVCkBj0Vhqsfea64Z4NYrk2k3EQAR73MvmlKGEVWZVVfXf"
+    }
+})
+.then (response => response.json())
+.then (immagini => { console.log(immagini) 
+        Visualizza(immagini)    
+})
+.catch (err => console.log(err))
 }
 
+function Visualizza(immagini) { 
+    const contenitore = document.getElementById("contenitore")
+    contenitore.innerHTML=""
+
+    immagini.photos.forEach(photo => {
+        const Card = document.createElement("div")
+        Card.classList.add("Card", "col-6", "col-md-4")
+
+        const image = document.createElement("img")
+        Card.classList.add("Card-img")
+        image.src = photo.src.medium
+
+        Card.appendChild(image); 
+        contenitore.appendChild(Card);
+    });
+}
+
+funzionefetch(); 
